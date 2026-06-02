71eada4d045d599c9ef938d25742f102ce5af355
junya <junya@dentalx.jp>
2026-06-03 05:22:34 +0900

    Initial Commit

---
+++ WellNews/WellNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellNewsApp.swift
+//  WellNews
+//
+//  Created by hondajunya on 2026/06/03.
+//
+
+import SwiftUI
+
+@main
+struct WellNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
